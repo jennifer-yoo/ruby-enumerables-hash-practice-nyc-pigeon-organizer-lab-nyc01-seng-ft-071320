7b7fb9e0152784073c8a2376bad6dc2369545199
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-11 02:39:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+=======
+require 'pry'
+>>>>>>> 2f3c7dbef897bbe056ce760e2262d731db1215f7
 
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
+<<<<<<< HEAD
    data.each do |attribute, detail|
     detail.each do |key, name|
       name.each do |pigeon_name|
@@ -14,6 +19,12 @@ def nyc_pigeon_organizer(data)
         pigeon_list[pigeon_name][attribute].push(key.to_s)
       end
     end
+=======
+  data.each do |key, value|
+    binding.pry
+>>>>>>> 2f3c7dbef897bbe056ce760e2262d731db1215f7
   end
 pigeon_list
 end
+
+binding.pry
